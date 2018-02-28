ceb3eae6dcd38b233c4262efd1ccbb350b7250cd
Kumar, Saurabh <krsaurabh.dev@gmail.com>
2018-03-01 01:46:56 +0530

    client and server

--- client.py
+++ client.py
@@ -1,5 +1,10 @@
-import RPi.GPIO as GPIO
+#!flask/bin/python
+
+# import RPi.GPIO as GPIO
 import time
+import cv2
+import requests
+import json
 
 classToLed = [24, 25, 8, 7, 12]
 
@@ -9,33 +14,54 @@ def switchOnLed(type):
 def switchOffLed(type):
         GPIO.output(classToLed[type], False)
 
+def resetLed():
+    for i in range(0, 4):
+        GPIO.output(classToLed[i], False)
+
+def clientCode():
+    addr = 'http://localhost:5000'
+    test_url = addr + '/api/getclass'
+
+    # prepare headers for http request
+    content_type = 'image/jpeg'
+    headers = {'content-type': content_type}
+
+    img = cv2.imread('lena.jpg')
+    # encode image as jpeg
+    _, img_encoded = cv2.imencode('.jpg', img)
+    # send http request with image and receive response
+    
+    response = requests.post(test_url, data=img_encoded.tostring(), headers=headers)
+    classObj = json.loads(response.text)
+    imgClass = classObj['class']
+
+    # decode response
+    return imgClass
+
+
 
 def main():
-        GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BCM)
 
-        #Button to GPIO23
-        GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    #Button to GPIO23
+    GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-        #Led
-        GPIO.setup(24, GPIO.OUT)
-        GPIO.setup(25, GPIO.OUT)  
-        GPIO.setup(8, GPIO.OUT)  
-        GPIO.setup(7, GPIO.OUT)  
-        GPIO.setup(12, GPIO.OUT)    
+    #Led
+    GPIO.setup(24, GPIO.OUT)
+    GPIO.setup(25, GPIO.OUT)  
+    GPIO.setup(8, GPIO.OUT)  
+    GPIO.setup(7, GPIO.OUT)  
+    GPIO.setup(12, GPIO.OUT)    
 
-        try:
-            while True:
-                 #button_state = GPIO.input(23)
-                 #if button_state == False:
-                 #   GPIO.output(24, True)
-                 #    print('Button Pressed...')
-                 #    time.sleep(0.2)
-                 #else:
-                 #    GPIO.output(24, False)
-                 for i in range(0, 5):
-                        switchOnLed(i)
-                        time.sleep(1)
-                        switchOffLed(i)
-        except:
-                GPIO.cleanup()
+    try:
+        while True:
+            button_state = GPIO.input(23)
+            if button_state == False:
+                resetLed()
+                imgClass = clientCode() 
+                switchOnLed(imgClass)
+    except:
+        GPIO.cleanup()
+
 main()
+
---
+++ server.py
@@ -0,0 +1,38 @@
+#!flask/bin/python
+from flask import Flask, request, Response
+import jsonpickle
+import numpy as np
+import cv2
+import random
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+def getClassFromImg(img):
+    return random.randint(0, 4)
+
+
+# route http posts to this method
+@app.route('/api/getclass', methods=['POST'])
+def getclass():
+    r = request
+    # convert string of image data to uint8
+    nparr = np.fromstring(r.data, np.uint8)
+    # decode image
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    # do some fancy processing here....
+
+    imgClass = getClassFromImg(img)
+    # build a response dict to send back to client
+    response = {
+            'class': '{}'.format(imgClass)
+                }
+    # encode response using jsonpickle
+    response_pickled = jsonpickle.encode(response)
+
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+    
+
+# start flask app
+app.run(host="0.0.0.0", port=5000)
